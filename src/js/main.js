e8c7215ebdd8670b906e4a40ddf0e4b6ad4031c2
00nx6 <lszabo958@gmail.com>
2022-03-29 16:02:15 +0200

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -100,3 +100,9 @@ const commentProfile = (objData) => {
 
     genCont.appendChild(commentBody)
 }
+
+// make a seperate function for appending the comment, 
+// so it can be reused for comments too
+// if in the forEach loop, it finds comments to be a non empty array, 
+// for every reply on the comment run the reply append function
+// determine if its a reply by passing it on as a boolean
